561f53dc77b688a3cce4080da4d94d17365089a2
Shruthi Sivaraman <shruthisivaraman25@gmail.com>
2026-02-24 20:48:38 +0530

    Final deployment fix

--- app.py
+++ app.py
@@ -135,5 +135,8 @@ def logout():
     return redirect(url_for('login'))
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
